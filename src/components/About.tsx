e28fb807ec62adf577b46239f866a0d0ffb9458a
nkserveren26 <norinoritoshi@outlook.jp>
2023-08-20 16:20:41 +0900

    commit

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -14,82 +14,13 @@ import typescript from "../img/typescript.jpg";
 import react from "../img/react.jpg";
 import vue from "../img/vue.jpg";
 import { Profile } from "./Profile";
+import { Skillset } from "./Skillset";
 
 export const About = () => {
     return (
         <>
         <Profile />
-        <SDiv>
-            <h1>スキルセット</h1>
-            <GridDiv>
-            <Grid container spacing={4} columns={{ xs: 6, md: 8 }} alignItems='center' justifyContent='center'>
-                <Grid item xs={6} md={4}>
-                    <Card>
-                        <CardContent>
-                            <Typography align="center" variant="h4">
-                              OS
-                            </Typography>
-                        </CardContent>
-                        <CardContent>
-                            <SDivFlex>
-                                <Avatar alt="windowsserver" src={windowsserver} sx={{ width: 100, height: 120 }} variant="square" />
-                                <Avatar alt="centos" src={centos} sx={{ width: 100, height: 110 }} variant="square" />
-                                <Avatar alt="ubuntu" src={ubuntu} sx={{ width: 100, height: 80 }} variant="square" />
-                            </SDivFlex>
-                        </CardContent>
-                    </Card>
-                </Grid>
-                    <Grid item xs={6} md={4}>
-                    <Card>
-                        <CardContent>
-                            <Typography align="center" variant="h4">
-                                Virtualization
-                            </Typography>
-                        </CardContent>
-                        <CardContent>
-                            <SDivFlex>
-                                <Avatar alt="vmware" src={vmware} sx={{ width: 140, height: 120 }} variant="square" />
-                                <Avatar alt="hyperv" src={hyperv} sx={{ width: 100, height: 120 }} variant="square" />
-                                <Avatar alt="vsphere" src={vsphere} sx={{ width: 100, height: 110 }} variant="square" />
-                            </SDivFlex>
-                        </CardContent>
-                    </Card>
-                </Grid>
-                    <Grid item xs={6}  md={4}>
-                    <Card>
-                        <CardContent>
-                            <Typography align="center" variant="h4">
-                                Programming
-                            </Typography>
-                        </CardContent>
-                        <CardContent>
-                            <SDivFlex>
-                                <Avatar alt="java" src={java} sx={{ width: 100, height: 120 }} variant="square" />
-                                <Avatar alt="python" src={python} sx={{ width: 100, height: 100 }} variant="square" />
-                                <Avatar alt="javascript" src={javascript} sx={{ width: 100, height: 80 }} variant="square" />
-                                <Avatar alt="typescript" src={typescript} sx={{ width: 100, height: 70 }} variant="square" />
-                            </SDivFlex>
-                        </CardContent>
-                    </Card>
-                </Grid>
-                <Grid item xs={6} md={4}>
-                    <Card>
-                        <CardContent>
-                            <Typography align="center" variant="h4">
-                                Frontend
-                            </Typography>
-                        </CardContent>
-                        <CardContent>
-                            <SDivFlex>
-                                <Avatar alt="react" src={react} sx={{ width: 130, height: 120 }} variant="square" />
-                                <Avatar alt="vue" src={vue} sx={{ width: 120, height: 100 }} variant="square" />
-                            </SDivFlex>
-                        </CardContent>
-                    </Card>
-                </Grid>
-            </Grid>
-            </GridDiv>   
-        </SDiv>
+        <Skillset />
         <SDiv>
             <h1>資格</h1>
         </SDiv>
@@ -97,27 +28,12 @@ export const About = () => {
     );
 };
 
-
 const SDiv = styled.div`
-background-color: gray;
+background-color: lightblue;
 margin: 0;
 height: 600px;
 h1 {
     text-align: center;
     margin: 0;
 }
-`;
-
-const GridDiv = styled.div`
-padding-left: 70px;
-padding-right: 70px;
-`;
-
-
-const SDivFlex = styled.div`
-display: flex;
-flex-basis: 50px;
-gap: 20px 20px;
-padding: 0px;
-justify-content: center;
 `;
---
+++ src/components/Skillset.tsx
@@ -0,0 +1,118 @@
+import styled from "styled-components";
+import { CardContent, Grid, Typography, Avatar } from "@mui/material";
+import { Card } from "@mui/material";
+import windowsserver from "../img/windowsserver.jpg";
+import centos from "../img/centos.jpg";
+import ubuntu from "../img/ubuntu.jpg";
+import vmware from "../img/vmware.jpg";
+import hyperv from "../img/hyper-v.jpg";
+import vsphere from "../img/vsphere.jpg";
+import java from "../img/java.jpg";
+import python from "../img/python.jpg";
+import javascript from "../img/javascript.jpg";
+import typescript from "../img/typescript.jpg";
+import react from "../img/react.jpg";
+import vue from "../img/vue.jpg";
+
+export const Skillset = () => {
+    return (
+        <>
+            <SDiv>
+                <h1>スキルセット</h1>
+                <GridDiv>
+                    <Grid container spacing={4} columns={{ xs: 6, md: 8 }} alignItems='center' justifyContent='center'>
+                        <Grid item xs={6} md={4}>
+                            <Card>
+                                <CardContent>
+                                    <Typography align="center" variant="h4">
+                                        OS
+                                    </Typography>
+                                </CardContent>
+                                <CardContent>
+                                    <SDivFlex>
+                                        <Avatar alt="windowsserver" src={windowsserver} sx={{ width: 100, height: 120 }} variant="square" />
+                                        <Avatar alt="centos" src={centos} sx={{ width: 100, height: 110 }} variant="square" />
+                                        <Avatar alt="ubuntu" src={ubuntu} sx={{ width: 100, height: 80 }} variant="square" />
+                                    </SDivFlex>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                        <Grid item xs={6} md={4}>
+                            <Card>
+                                <CardContent>
+                                    <Typography align="center" variant="h4">
+                                        Virtualization
+                                    </Typography>
+                                </CardContent>
+                                <CardContent>
+                                    <SDivFlex>
+                                        <Avatar alt="vmware" src={vmware} sx={{ width: 140, height: 120 }} variant="square" />
+                                        <Avatar alt="hyperv" src={hyperv} sx={{ width: 100, height: 120 }} variant="square" />
+                                        <Avatar alt="vsphere" src={vsphere} sx={{ width: 100, height: 110 }} variant="square" />
+                                    </SDivFlex>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                        <Grid item xs={6} md={4}>
+                            <Card>
+                                <CardContent>
+                                    <Typography align="center" variant="h4">
+                                        Programming
+                                    </Typography>
+                                </CardContent>
+                                <CardContent>
+                                    <SDivFlex>
+                                        <Avatar alt="java" src={java} sx={{ width: 100, height: 120 }} variant="square" />
+                                        <Avatar alt="python" src={python} sx={{ width: 100, height: 100 }} variant="square" />
+                                        <Avatar alt="javascript" src={javascript} sx={{ width: 100, height: 80 }} variant="square" />
+                                        <Avatar alt="typescript" src={typescript} sx={{ width: 100, height: 70 }} variant="square" />
+                                    </SDivFlex>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                        <Grid item xs={6} md={4}>
+                            <Card>
+                                <CardContent>
+                                    <Typography align="center" variant="h4">
+                                        Frontend
+                                    </Typography>
+                                </CardContent>
+                                <CardContent>
+                                    <SDivFlex>
+                                        <Avatar alt="react" src={react} sx={{ width: 130, height: 120 }} variant="square" />
+                                        <Avatar alt="vue" src={vue} sx={{ width: 120, height: 100 }} variant="square" />
+                                    </SDivFlex>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    </Grid>
+                </GridDiv>
+            </SDiv>
+        </>
+    )
+}
+
+const SDiv = styled.div`
+background-color: gray;
+margin: 0;
+height: 600px;
+h1 {
+    text-align: center;
+    margin: 0;
+    padding-top: 20px;
+    padding-bottom: 20px;
+}
+`;
+
+const GridDiv = styled.div`
+padding-left: 70px;
+padding-right: 70px;
+`;
+
+const SDivFlex = styled.div`
+display: flex;
+flex-basis: 50px;
+gap: 20px 20px;
+padding: 0px;
+justify-content: center;
+`;

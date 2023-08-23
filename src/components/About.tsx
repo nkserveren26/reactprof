76a1d3c11809a6957d1a034c9880a85088116565
nkserveren26 <norinoritoshi@outlook.jp>
2023-08-23 22:02:41 +0900

    commit

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,21 +1,25 @@
 import styled from "styled-components";
-import { CardContent, Grid, Typography,Avatar } from "@mui/material";
+import { CardContent, Grid, Typography, } from "@mui/material";
 import { Card } from "@mui/material";
-import windowsserver from "../img/windowsserver.jpg";
-import centos from "../img/centos.jpg";
-import ubuntu from "../img/ubuntu.jpg";
-import vmware from "../img/vmware.jpg";
-import hyperv from "../img/hyper-v.jpg";
-import vsphere from "../img/vsphere.jpg";
-import java from "../img/java.jpg";
-import python from "../img/python.jpg";
-import javascript from "../img/javascript.jpg";
-import typescript from "../img/typescript.jpg";
-import react from "../img/react.jpg";
-import vue from "../img/vue.jpg";
 import { Profile } from "./Profile";
 import { Skillset } from "./Skillset";
 import { CertificationList } from "./CertificationList";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+    components: {
+        // コンポーネント名
+        MuiCardContent: {
+            styleOverrides: {
+                // CSSルール名
+                root: {
+                    // CSS定義
+                    padding: 0,
+                },
+            },
+        },
+    },
+});
 
 export const About = () => {
     const awsCertifications: string[] = [
@@ -32,14 +36,14 @@ export const About = () => {
                     <Grid container spacing={4} columns={{ xs: 6, md: 8 }} alignItems='center' justifyContent='center'>
                         <Grid item xs={6} md={4}>
                             <Card>
-                                <CardContent>
+                                <HogeCard>
                                     <Typography align="center" variant="h4">
                                         AWS
                                     </Typography>
-                                </CardContent>
-                                <CardContent>
+                                </HogeCard>
+                                <HogeCard>
                                     <CertificationList list={awsCertifications} />
-                                </CardContent>
+                                </HogeCard>
                             </Card>
                         </Grid>
                     </Grid>
@@ -63,6 +67,11 @@ const GridDiv = styled.div`
 padding-left: 5%;
 padding-right: 5%;
 `;
+
+const HogeCard = styled(CardContent)`
+padding: 0;
+padding-bottom: 0;
+`
 
 const SDivFlex = styled.div`
 display: flex;

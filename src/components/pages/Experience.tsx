1ccab025ee4a45c90c641b0ab765bb47d1b2916c
nkserveren26 <norinoritoshi@outlook.jp>
2023-08-26 16:28:23 +0900

    commit

--- src/components/pages/Experience.tsx
+++ src/components/pages/Experience.tsx
@@ -4,8 +4,10 @@ export const Experience = () => {
     return (
         <>
         <SDiv>
-            <h4>COMPANY</h4>
+            <h2>COMPANY</h2>
             <p>株式会社KYOSO - AWS Enginner (2022/8 ~ 現在) </p>
+            <p>株式会社シーエーシー - Server Side Engineer (2021/4 ~ 2022/7) </p>
+            <p>ESRIジャパン株式会社 - Infrastructure Engineer (2018/4 ~ 2021/3) </p>
         </SDiv>
         </>
     )

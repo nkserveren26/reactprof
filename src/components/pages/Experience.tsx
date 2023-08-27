966412ad19cdc305a5c444b6584703542112f43b
nkserveren26 <norinoritoshi@outlook.jp>
2023-08-27 14:05:25 +0900

    commit

--- src/components/pages/Experience.tsx
+++ src/components/pages/Experience.tsx
@@ -1,14 +1,18 @@
 import styled from "styled-components"
+import Box from '@mui/material/Box';
 
 export const Experience = () => {
     return (
         <>
-        <SDiv>
+         <Box sx={{
+            height: 200,
+            backgroundColor: 'white',
+        }}>
             <h2>COMPANY</h2>
             <p>株式会社KYOSO - AWS Enginner (2022/8 ~ 現在) </p>
             <p>株式会社シーエーシー - Server Side Engineer (2021/4 ~ 2022/7) </p>
             <p>ESRIジャパン株式会社 - Infrastructure Engineer (2018/4 ~ 2021/3) </p>
-        </SDiv>
+        </Box>
         </>
     )
 }

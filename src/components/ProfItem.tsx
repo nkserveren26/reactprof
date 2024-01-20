5653830325ad18fcbcc3204dba6304a92a86a6ee
nkserveren26 <norinoritoshi@outlook.jp>
2024-01-20 21:39:14 +0900

    ProfItemの不要コードを削除。 #7

--- src/components/ProfItem.tsx
+++ src/components/ProfItem.tsx
@@ -10,18 +10,9 @@ type Props = {
 
 export const ProfItem: React.FC<ProfItemProps> = (props) => {
     const { icon, title, subheader } = props;
-    const getItemWidth = () => {
-        let itemWidth: number = 0;
-        if (window.innerWidth <= 800) {
-            itemWidth = 150;
-        } else {
-            itemWidth = 300;
-        }
-        return itemWidth;
-    }
-    const profItemWidth: number = getItemWidth();
+
     return (
-        <Card sx={{ width: profItemWidth, height: 90 }} >
+        <Card sx={{ width: 300, height: 90 }} >
             <CardHeader
               avatar={<Avatar>{icon}</Avatar>}
               title={title}

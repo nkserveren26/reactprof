2591b3eb793efae6130af37c9a349da99efa8f18
nkserveren26 <norinoritoshi@outlook.jp>
2024-01-27 23:59:20 +0900

    ScrollToTopに型を追加。 #7

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-export const ScrollTop = () => {
+export const ScrollTop: React.FC = () => {
     const { pathname } = useLocation();
 
     useEffect(() => {

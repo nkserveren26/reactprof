3878dd6978a8151373b678b3a18ee1810601f982
nkserveren26 <norinoritoshi@outlook.jp>
2024-02-02 23:53:09 +0900

    Topページのレイアウト調整。

--- src/components/pages/Top.tsx
+++ src/components/pages/Top.tsx
@@ -1,78 +1,48 @@
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 import background from "../../img/IMG_4871.jpg";
 import snoopy from "../../img/IMG_7330.jpg";
 import React from 'react';
+import { Box, Grid, Typography, createTheme } from '@mui/material';
+
+
 
 export const Top = () => {
+  const customFontFamily = 'Palatino';
     return (
-        <div className="App">
-          <SDivTop>
-            <div className='snoopy'>
-              <img src={snoopy}></img>
-            </div>
-            <div className='mysummary'>
-              <h1>IT Engineer</h1>
-              <h2>N.K</h2>
-              <p>AWSエンジニアやってます。<br />
-                バックエンドAPIの開発やAWSリソースの構築・運用をしています。<br />
-                プライベートは資格の勉強したり、コード書いたりしてます。<br />
-              </p>
-            </div>
-          </SDivTop>
-        </div>
+          <SBox>
+          <Grid container columns={{ xs: 6, sm: 8, md: 12 }} alignItems='center' justifyContent='center'>
+            <HiddenOnSmallScreen item  md="auto" pt={8} >
+              <img src={snoopy} alt="Top Page Pictures" height="270px" />
+            </HiddenOnSmallScreen>
+            <Grid item md="auto" pl={4}>
+                <Typography pt={8} fontWeight="bold" variant="h2" style={{ fontFamily: customFontFamily }}>
+                  IT Engineer
+                </Typography>
+                <Typography pt={1} fontWeight="bold" variant="h3" >
+                  N.K
+                </Typography>
+                  <Typography pt={3} fontWeight="bold" variant="body1" >
+                    現在はAWSエンジニアをやってます。<br />
+                    AWS基盤上でのバックエンド、フロントエンドの開発や、<br />
+                    AWSリソースの構築・運用をしています。<br />
+                    過去にはオンプレ環境での仮想化基盤構築やJavaの開発もやってました。<br />
+                    プライベートは資格の勉強したり、コード書いたりしてます。<br />
+                </Typography>
+            </Grid>
+          </Grid>
+          </SBox>
       );
 };
 
-const SDivTop = styled.div`
-position: relative;
-z-index: 1;
-display: flex;
-column-gap: 5px;
+const HiddenOnSmallScreen = styled(Grid)`
+z-index: 3;
+  @media (max-width: 800px) {
+    display: none;
+  }
+`;
+
+const SBox = styled(Box)`
 background-image: url(${background});
 background-size: cover;
-height: 700px;
-::after {
-  content: "";
-  position: absolute;
-  z-index: 2;
-  width: 100%;
-  height:100%;
-  top: 0;
-  left: 0;
-  background-color: white;
-  opacity: 0.5;
-}
-.snoopy {
-  position: relative;
-  z-index: 3;
-  top: 15%;
-  left: 10%;
-  height: 300px;
-  width: 400px;
-  margin: 0;
-  img {
-    width: 100%;
-  }
-}
-.mysummary {
-  position: relative;
-  z-index: 3;
-  top: 15%;
-  left: 20%;
-  height: 60%;
-  h1 {
-  font-family: 'Courier New', Courier, monospace;
-  margin: 0;
-  font-size: 50px;
-  }
-  h2 {
-    font-family: 'Courier New', Courier, monospace;
-    margin: 0;
-    font-size: 110px;
-  }
-  p {
-    margin: 0;
-    font-weight: bold;
-  }
-}
+height: 600px;
 `;

e24c6bfeed85762df1058e2bb7ec0c447802db31
nkserveren26 <norinoritoshi@outlook.jp>
2024-02-25 23:14:57 +0900

    大枠の実装。#43

--- src/components/pages/Blog.tsx
+++ src/components/pages/Blog.tsx
@@ -1,7 +1,19 @@
+import { Box, Typography } from "@mui/material";
 import React from "react";
 
 export const Blog: React.FC = () => {
     return (
-        <></>
+        <>
+            <Box
+                display="flex"
+                flexDirection="column"
+                alignItems="center"
+                justifyContent="center"
+                m={0} pt={5} pl={2} pr={2} sx={{
+                    backgroundColor: 'white',
+            }}>
+                <Typography paddingBottom={3} fontWeight="bold" variant="h3">My Tech Blogs</Typography>
+            </Box>
+        </>
     )
 }
